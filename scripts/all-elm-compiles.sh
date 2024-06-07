8f38867adf2f7e7caad82608b0ab4a557de2346a
Vincent Jousse <vincent@jousse.org>
2024-06-07 08:18:27 +0200

    chore: add CI and pre-commit

---
+++ scripts/all-elm-compiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+ROOT_DIR=$(dirname $SCRIPT_DIR)
+cd $ROOT_DIR
+MAINS=$(find ./src/* -type f -name Main.elm)
+
+RESULT=0
+for MAIN in $MAINS; do
+	elm make --optimize $MAIN --output=/dev/null >/dev/null
+	RESULT=$((RESULT + $?))
+done
+if (($RESULT != 0)); then
+	echo "FAILED"
+else
+	echo "All elm compiles"
+fi
